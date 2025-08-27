8ca2dc64ca1190d956799b9c56a224a1183400bc
muftawu-ubuntu <muftawu@multipass.com>
2025-08-27 10:27:59 +0000

    added command line arg support

--- rot.py
+++ rot.py
@@ -1,9 +1,11 @@
-alphabets_latin = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "x", "y", "z"]
-alphabets_english = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+import sys
+
+alphabets_latin = list("abcdefghijklmnopqrstvxyz")
+alphabets_english = list("abcdefghijklmnopqrstuvwxyz")
 cutoff = 12
 
 def get_rot13(letter: str, alphabet_type: str = "latin"):
-    alphabets = alphabets_latin if type == "lating" else alphabets_english  
+    alphabets = alphabets_latin if type == "latin" else alphabets_english  
     out = None
 
     if not isinstance(letter, str):
@@ -17,18 +19,26 @@ def get_rot13(letter: str, alphabet_type: str = "latin"):
 
     try:
         letter_index = alphabets.index(letter)
-        if letter_index:
-            letters_before = alphabets[:letter_index]
+        if letter_index.is_integer():
+            letters_before = alphabets[:letter_index+1] 
             letters_after = alphabets[letter_index+1:]
             working_array = letters_after if len(letters_after) >= 13 else letters_after + letters_before
-            out = working_array[cutoff]
+            out = working_array[cutoff] if working_array[cutoff] is not None else alphabets[cutoff]
     except Exception as e:
         print(f"Failed to get rot13 for {letter} => {str(e)}")
     return out
 
 if __name__ == "__main__":
-    letter = "muftawu"
-    [print(f"{let} => {get_rot13(let, 'eng')}", sep=" ") for let in letter]
+    if len(sys.argv) > 2:
+        print(f"Running rot13 for {sys.argv[1]} with type {sys.argv[2]}")
+        letter = sys.argv[1]
+        alphabet_type = sys.argv[2]
+    else:
+        print("sys args less than 2. Using default string 'hello' with type 'latin'")
+        letter = "hello"
+        alphabet_type = "latin"
+    res = ''.join([f"{get_rot13(let, {alphabet_type})}" for let in letter])
+    print(f"{letter} => {res}")
     
 
 
